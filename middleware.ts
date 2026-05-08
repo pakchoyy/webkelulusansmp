2463fbf266a2ae498722a3a3b169536f641f8439
Pak Choyy <pulsachoy@gmail.com>
2026-05-08 09:05:43 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -33,7 +33,7 @@ export async function middleware(request: NextRequest) {
     request.nextUrl.pathname === '/login' ||
     request.nextUrl.pathname === '/register'
   )) {
-    return NextResponse.redirect(new URL('/dashboard', request.url))
+    return NextResponse.redirect(new URL('/b2g4y10-enroll', request.url))
   }
 
   return supabaseResponse
